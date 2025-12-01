c0c5935cdbb58a37f95348afb10399738931f210
Estefania Rodriguez <105613482+EstefaniaJRodriguez@users.noreply.github.com>
2025-12-01 20:07:49 -0300

    recategorizacion

--- src/Components/Recategorizacion.jsx
+++ src/Components/Recategorizacion.jsx
@@ -268,7 +268,7 @@ const RecategorizacionMonotributoForm = () => {
           <div className="text-center mt-4 mb-3">
             <p className="fw-bold">Monto a abonar por la gestión del trámite: ${precioTramite}</p>
         
-            <p className="fw-bold">Total a pagar por la gestión del trámite: ${total.toLocaleString("es-AR")}</p>
+           
           </div>
 
           {/* Botón */}
